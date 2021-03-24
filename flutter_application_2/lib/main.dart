30fec41d69698c22cc32872a7dfbee9ad3e328f6
Nightgg42 <nightxshot29@gmail.com>
2021-03-24 11:00:40 +0700

    New file

---
+++ flutter_application_2/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_application_2/api/firebase_api.dart';
+import 'package:flutter_application_2/page/chat_page.dart';
+import 'package:flutter_application_2/page/chats_page.dart';
+import 'package:flutter_application_2/users.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseApi.addRandomUsers(Users.initUsers);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Firebase Chat';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: ChatsPage(),
+      );
+}
